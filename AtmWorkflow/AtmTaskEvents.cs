ba9dbf3f883db45e590f62aed1941a3d6f5fd5d3
hmirosi <h.mirosi@gmail.com>
2021-06-15 22:57:44 +0200

    Task events for Atm workflow defined.

---
+++ AtmWorkflow/AtmTaskEvents.cs
@@ -0,0 +1,17 @@
+﻿using WorkflowManager.Core.Enums;
+
+namespace AtmWorkflow
+{
+    internal class AtmTaskEvents : TaskEventEnum
+    {
+        public static AtmTaskEvents DataEntered = new AtmTaskEvents(1, "DataEntered");
+        public static AtmTaskEvents TransferSelected = new AtmTaskEvents(2, "TransferSelected");
+        public static AtmTaskEvents WithdrawSelected = new AtmTaskEvents(3, "WithdrawSelected");
+        public static AtmTaskEvents InvalidInputEntered = new AtmTaskEvents(4, "InvalidInputEntered");
+        public static AtmTaskEvents OtherOptionsSelected = new AtmTaskEvents(5, "OtherOptionsSelected");
+
+        public AtmTaskEvents(int id, string name) : base(id, name)
+        {
+        }
+    }
+}
