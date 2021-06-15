c662961b25f2fb842fffb097cc591c8164d41e38
hmirosi <h.mirosi@gmail.com>
2021-06-15 22:52:38 +0200

    Task types for Atm workflow created.

---
+++ AtmWorkflow/AtmTaskTypes.cs
@@ -0,0 +1,21 @@
+﻿using WorkflowManager.Core.Enums;
+
+namespace AtmWorkflow
+{
+    internal class AtmTaskTypes : TaskTypeEnum
+    {
+        public static AtmTaskTypes SelectRequest = new AtmTaskTypes(1, "SelectRequest");
+        public static AtmTaskTypes SelectAmount = new AtmTaskTypes(2, "SelectAmount");
+        public static AtmTaskTypes SelectOtherAmount = new AtmTaskTypes(3, "SelectOtherAmount");
+        public static AtmTaskTypes SelectDestinationAccount = new AtmTaskTypes(4, "SelectDestinationAccount");
+        public static AtmTaskTypes EnterPin = new AtmTaskTypes(5, "EnterPin");
+        public static AtmTaskTypes CheckRequestType = new AtmTaskTypes(6, "CheckRequestType");
+        public static AtmTaskTypes Transfer = new AtmTaskTypes(7, "Transfer");
+        public static AtmTaskTypes Withdraw = new AtmTaskTypes(8, "Withdraw");
+        public static AtmTaskTypes ServiceUnavailable = new AtmTaskTypes(9, "ServiceUnavailable");
+
+        public AtmTaskTypes(int id, string name) : base(id, name)
+        {
+        }
+    }
+}
