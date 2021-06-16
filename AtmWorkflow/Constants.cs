00a469d16275994a699c1d201ce8e8456078e4b9
hmirosi <h.mirosi@gmail.com>
2021-06-16 23:59:27 +0200

    A class defined for constants to be used all over the app.

---
+++ AtmWorkflow/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace AtmWorkflow
+{
+    class Constants
+    {
+        public const string UserRequest = "UserRequest";
+        public const string TransferSelected = "Transfer";
+        public const string WithdrawSelected = "Withdraw";
+    }
+}
