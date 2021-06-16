c0fbf0ddeb859458d5279384e65eeb47e712fe0e
hmirosi <h.mirosi@gmail.com>
2021-06-16 23:58:04 +0200

    A base class created for workflow tasks.

---
+++ WorkflowManager.Core/BaseTypes/BaseWorkflowTask.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using WorkflowManager.Core.Enums;
+using WorkflowManager.Core.Interfaces;
+
+namespace WorkflowManager.Core.BaseTypes
+{
+    public abstract class BaseWorkflowTask : IWorkflowTask
+    {
+        protected readonly IContextParameterProvider _ContextParameterProvider;
+
+        public BaseWorkflowTask(IContextParameterProvider contextParameterProvider)
+        {
+            _ContextParameterProvider = contextParameterProvider;
+        }
+
+        public abstract TaskTypeEnum Type { get; }
+
+        public abstract Task<ITaskResult> HandleAsync(params object[] inputArgs);
+    }
+}
