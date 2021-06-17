c940f3f2478f5e8aba5829b04ce0fc38c0f25e9c
hmirosi <h.mirosi@gmail.com>
2021-06-17 08:36:17 +0200

    Task implemented.

--- AtmWorkflow/Tasks/SelectOtherAmount.cs
+++ AtmWorkflow/Tasks/SelectOtherAmount.cs
@@ -12,13 +12,25 @@ namespace AtmWorkflow.Tasks
     {
         public SelectOtherAmount(IContextParameterProvider contextParameterProvider) : base(contextParameterProvider)
         {
+            Console.WriteLine("Please specify your desired amount:");
         }
 
         public override TaskTypeEnum Type => AtmTaskTypes.SelectOtherAmount;
 
         public override Task<ITaskResult> HandleAsync(params object[] inputArgs)
         {
-            throw new NotImplementedException();
+            string userInput = Convert.ToString(inputArgs[0]).Trim();
+
+            if (int.TryParse(userInput, out int selectedAmount))
+            {
+                if (selectedAmount < 500)
+                {
+                    _ContextParameterProvider.WriteParameter(Constants.SelectedAmount, selectedAmount);
+                    return Task.FromResult(this.GetTaskResult(AtmTaskEvents.DataEntered));
+                }
+            }
+
+            return Task.FromResult(this.GetTaskResult(AtmTaskEvents.InvalidInputEntered));
         }
     }
 }
